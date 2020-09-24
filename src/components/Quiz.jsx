4a7c0798710d562fae2d83284558263276c8e53e
Suzana-Melo-Moraes <suzanamelojor@gmail.com>
2020-09-25 11:55:50 +1200

    import and receive database, map the data and send it to the Question child component

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import Question from './Question';
 
+import db from '../db';
+const questions = db.questions;
+
 const Quiz = () => {
   return (
     <div>
       <h1>Welcome to Harry Potter web quiz </h1>
-      <Question />
+      <form>
+        <ol>
+          {questions.map((question) => (
+            <li key={question.id}>
+              <Question question={question} />
+            </li>
+          ))}
+        </ol>
+      </form>
     </div>
   );
 };
