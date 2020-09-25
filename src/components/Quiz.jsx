bf4c4a03f70b35546dc0b6d74f70376ba27986ab
Suzana-Melo-Moraes <suzanamelojor@gmail.com>
2020-09-25 16:01:44 +1200

    create a function that receives a id and answer-value, check if the value match the correct answer from the data base and returns the result to the user when the button is submited

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,18 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Question from './Question';
 
 import db from '../db';
-const questions = db.questions;
-
-const handleAnswerChange = ({ id, value }) => {
-  console.log('answer:', id, value);
-};
 
 const Quiz = () => {
+  const questions = db.questions;
+
+  const initialCount = 0;
+  const [correctAnswers, setCorrectAnswers] = useState(initialCount);
+
+  const handleAnswerChange = ({ id, value }) => {
+    questions.map((question) => {
+      if (question.id === id) {
+        const [a, b] = [question.answer, value].map((x) => JSON.stringify(x));
+
+        const isCorrect = a === b;
+
+        if (isCorrect) {
+          setCorrectAnswers((prevState) => prevState + 1);
+        }
+      }
+    });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert(`you've answered ${correctAnswers}/${questions.length} correctly!`);
+  };
+
   return (
     <div>
       <h1>Welcome to Harry Potter web quiz </h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <ol>
           {questions.map((question) => (
             <li key={question.id}>
@@ -23,6 +42,7 @@ const Quiz = () => {
             </li>
           ))}
         </ol>
+        <button type='submit'>Submit</button>
       </form>
     </div>
   );
