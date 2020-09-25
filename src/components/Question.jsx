4ccc0ae59b559188a26ab8e46cdb1df8037a1b2e
Suzana-Melo-Moraes <suzanamelojor@gmail.com>
2020-09-25 12:31:07 +1200

    create a variable called ANSWER_TYPE_COMPONENT_MAP to receive and define an object with properties related to the type of answers. It will help us to manage componentes created to display and receive inputs based on the type of the answers of the question. Create a child component called QuestionComponent to display the inputs components based on the type of the answer and assign it to the result of variable that already mapped the type of the answers by question. Create the input components  mapped inside the const ANSWER_TYPE_COMPONENT_MAP. Each input component will be built based in the type of the answers to display the correct input to each type

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,7 +1,79 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
+const handleValueChange = (event) => {
+  event.persist();
+  console.log(event.target.value);
+};
+
+const TextAnswer = () => {
+  return <div></div>;
+};
+
+const RadioAnswer = () => {
+  return <div></div>;
+};
+
+const SelectAnswer = () => {
+  return <div></div>;
+};
+
+const CheckboxAnswer = () => {
+  return <div></div>;
+};
+
+const RangeAnswer = () => {
+  return <div></div>;
+};
+
+// STRATEGY PATTERN
+const ANSWER_TYPE_COMPONENT_MAP = {
+  text: TextAnswer,
+  radio: RadioAnswer,
+  select: SelectAnswer,
+  checkbox: CheckboxAnswer,
+  range: RangeAnswer,
+};
 
 const Question = ({ question }) => {
-  return <div>{question.question} </div>;
+  const QuestionComponent = ANSWER_TYPE_COMPONENT_MAP[question.answerType];
+
+  return (
+    <div>
+      <div>{question.title}</div>
+
+      <div>
+        <QuestionComponent question={question} />
+      </div>
+    </div>
+  );
+};
+
+Question.propTypes = {
+  question: PropTypes.oneOf([
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      title: PropTypes.string.isRequired,
+      answer: PropTypes.oneOf([
+        PropTypes.string,
+        PropTypes.arrayOf(PropTypes.string),
+      ]).isRequired,
+      answerType: PropTypes.oneOf([
+        'text',
+        'radio',
+        'select',
+        'checkbox',
+        'range',
+      ]).isRequired,
+      answerOptions: PropTypes.oneOf([
+        PropTypes.arrayOf(PropTypes.string),
+        PropTypes.shape({
+          from: PropTypes.number.isRequired,
+          to: PropTypes.number.isRequired,
+        }),
+      ]),
+    }),
+  ]),
 };
 
 export default Question;
